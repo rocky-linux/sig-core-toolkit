0b4cab9adb5ce518b76f315a6bc0eb926e431d4b
nazunalika <label@rockylinux.org>
2021-07-11 15:50:45 -0700

    temporary file list modification

--- sync/sync-to-prod.sh
+++ sync/sync-to-prod.sh
@@ -22,5 +22,11 @@ if [ $ret_val -eq "0" ]; then
   # Full file list update
   cd "${PRODUCTION_ROOT}/${CATEGORY_STUB}/" || { echo "Failed to change directory"; exit 1; }
   find . > fullfilelist
+  if [[ -f /usr/local/bin/create-filelist ]]; then
+    # We're already here, but Justin Case wanted this
+    cd "${PRODUCTION_ROOT}/${CATEGORY_STUB}/" || { echo "Failed to change directory"; exit 1; }
+    /bin/cp fullfiletimelist-rocky fullfiletimelist-rocky-old
+    /usr/local/bin/create-filelist > fullfiletimelist-rocky
+  fi
 fi
 
