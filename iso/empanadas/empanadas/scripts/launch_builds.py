4587287e1e09e817b9cfa37e67129335ed387b6a
Neil Hanlon <neil@rockylinux.org>
2022-06-17 17:56:08 -0400

    Github actions work for automatic building
    * Add in really simple job template creation to prove the generation will
    work

---
+++ iso/empanadas/empanadas/scripts/launch_builds.py
@@ -0,0 +1,45 @@
+# Launches the builds of ISOs
+
+import argparse
+
+from empanadas.common import *
+from empanadas.common import _rootdir
+
+from jinja2 import Environment, FileSystemLoader
+
+parser = argparse.ArgumentParser(description="ISO Compose")
+
+parser.add_argument('--release', type=str, help="Major Release Version", required=True)
+parser.add_argument('--env', type=str, help="environment", required=True)
+results = parser.parse_args()
+rlvars = rldict[results.release]
+major = rlvars['major']
+
+EXTARCH=["s390x", "ppc64le"]
+EKSARCH=["amd64", "arm64"]
+
+def run():
+    file_loader = FileSystemLoader(f"{_rootdir}/templates")
+    tmplenv = Environment(loader=file_loader)
+    job_template = tmplenv.get_template('kube/Job.tmpl')
+
+    arches = EKSARCH
+    if results.env == "ext" and results.env != "all":
+        arches = EXTARCH
+    elif results.env == "all":
+        arches = EKSARCH+EXTARCH
+
+    out = ""
+    for arch in arches:
+        out += job_template.render(
+            architecture=arch,
+            backoffLimit=4,
+            command=["build-iso", "--release", "9", "--rc", "--isolation", "simple"],
+            containerName="buildiso",
+            imageName="ghcr.io/neilhanlon/sig-core-toolkit:latest",
+            jobName=f"build-iso-{arch}",
+            namespace="empanadas",
+            restartPolicy="Never",
+        )
+
+    print(out)
