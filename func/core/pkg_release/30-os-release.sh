5667a75fa22d63df26e9be1a5e183a48437c925c
nazunalika <label@rockylinux.org>
2022-06-21 20:29:07 -0700

    fix os release detection

--- func/core/pkg_release/30-os-release.sh
+++ func/core/pkg_release/30-os-release.sh
@@ -5,7 +5,7 @@ r_log "rocky" "Verify support directives"
 for s in NAME=\"Rocky\ Linux\" \
   ID=\"rocky\" \
   ROCKY_SUPPORT_PRODUCT=\"Rocky-Linux-$RL_VER\" \
-  ROCKY_SUPPORT_PRODUCT_VERSION=\"$RL_VER\"; do
+  ROCKY_SUPPORT_PRODUCT_VERSION=\"$RL_VER\..*\"; do
   if ! grep -q "$s" /etc/os-release; then
     r_log "rocky" "Missing string ($s) in /etc/os-release"
     r_checkExitStatus 1
