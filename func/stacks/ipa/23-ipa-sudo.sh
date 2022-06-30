1acca22141b22ff9d3063b931ee8fb52155c1cc3
Louis Abel <label@rockylinux.org>
2022-06-29 20:45:58 -0700

    make kinit as admin simpler

--- func/stacks/ipa/23-ipa-sudo.sh
+++ func/stacks/ipa/23-ipa-sudo.sh
@@ -13,15 +13,7 @@ kdestroy &> /dev/null
 klist 2>&1  | grep -E "(No credentials|Credentials cache .* not found)" &> /dev/null
 r_checkExitStatus $?
 
-expect -f - <<EOF
-set send_human {.1 .3 1 .05 2}
-spawn kinit admin
-sleep 1
-expect "Password for admin@RLIPA.LOCAL:"
-send -h "b1U3OnyX!\r"
-sleep 5
-close
-EOF
+echo "b1U3OnyX!" | kinit admin@RLIPA.LOCAL
 
 klist | grep "admin@RLIPA.LOCAL" &> /dev/null
 r_checkExitStatus $?
