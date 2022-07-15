aec6a5d52dad75ea1ca2a583ed254206a8e2eabc
Louis Abel <label@rockylinux.org>
2022-07-15 15:39:14 -0700

    separate file list generation from the rest

---
+++ sync/sync-full-file-list.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Syncs everything from staging to production
+
+# Source common variables
+# shellcheck disable=SC2046,1091,1090
+source "$(dirname "$0")/common"
+
+REV=${REVISION}${APPEND_TO_DIR}
+
+cd "${STAGING_ROOT}/${CATEGORY_STUB}/${REV}" || { echo "Failed to change directory"; ret_val=1; exit 1; }
+ret_val=$?
+
+if [ $ret_val -eq "0" ]; then
+  # Full file list update for production root
+  cd "${PRODUCTION_ROOT}/" || echo { echo "Failed to change directory"; exit 1; }
+  find . > fullfilelist
+  if [[ -f /usr/local/bin/create-filelist ]]; then
+    # We're already here, but Justin Case wanted this
+    cd "${PRODUCTION_ROOT}/" || { echo "Failed to change directory"; exit 1; }
+    /bin/cp fullfiletimelist-rocky fullfiletimelist-rocky-old
+    /usr/local/bin/create-filelist > fullfiletimelist-rocky
+    cp fullfiletimelist-rocky fullfiletimelist
+  fi
+  # Full file list update for rocky linux itself
+  cd "${PRODUCTION_ROOT}/${CATEGORY_STUB}/" || { echo "Failed to change directory"; exit 1; }
+  # Hardlink everything except xml files
+  hardlink -x '.*\.xml.*' "${REVISION}"
+  find . > fullfilelist
+  if [[ -f /usr/local/bin/create-filelist ]]; then
+    # We're already here, but Justin Case wanted this
+    cd "${PRODUCTION_ROOT}/${CATEGORY_STUB}/" || { echo "Failed to change directory"; exit 1; }
+    /bin/cp fullfiletimelist-rocky fullfiletimelist-rocky-old
+    /usr/local/bin/create-filelist > fullfiletimelist-rocky
+    cp fullfiletimelist-rocky fullfiletimelist
+  fi
+  chown 10004:10005 fullfilelist fullfiletimelist-rocky fullfiletimelist
+fi
+
--- sync/sync-to-prod-9.sh
+++ sync/sync-to-prod-9.sh
@@ -17,30 +17,30 @@ if [ $ret_val -eq "0" ]; then
   sudo -l && time fpsync -n 24 -o '-a --numeric-ids --no-compress --chown=10004:10005' -t /mnt/compose/partitions "${STAGING_ROOT}/${CATEGORY_STUB}/${REV}/" "${TARGET}/"
 
   # Full file list update for production root
-  cd "${PRODUCTION_ROOT}/" || { echo "Failed to change directory"; exit 1; }
-  echo "Getting a full file list for the root dir"
-  find . > fullfilelist
-  if [[ -f /usr/local/bin/create-filelist ]]; then
-    # We're already here, but Justin Case wanted this
-    cd "${PRODUCTION_ROOT}/" || { echo "Failed to change directory"; exit 1; }
-    /bin/cp fullfiletimelist-rocky fullfiletimelist-rocky-old
-    /usr/local/bin/create-filelist > fullfiletimelist-rocky
-    cp fullfiletimelist-rocky fullfiletimelist
-  fi
-  # Full file list update for rocky linux itself
-  cd "${PRODUCTION_ROOT}/${CATEGORY_STUB}/" || { echo "Failed to change directory"; exit 1; }
-  # Hardlink everything except xml files
-  echo "Hard linking"
-  hardlink -x '.*\.xml.*' "${REVISION}"
-  echo "Getting a full file list for the rocky dir"
-  find . > fullfilelist
-  if [[ -f /usr/local/bin/create-filelist ]]; then
-    # We're already here, but Justin Case wanted this
-    cd "${PRODUCTION_ROOT}/${CATEGORY_STUB}/" || { echo "Failed to change directory"; exit 1; }
-    /bin/cp fullfiletimelist-rocky fullfiletimelist-rocky-old
-    /usr/local/bin/create-filelist > fullfiletimelist-rocky
-    cp fullfiletimelist-rocky fullfiletimelist
-  fi
-  chown 10004:10005 fullfilelist fullfiletimelist-rocky fullfiletimelist
+  #cd "${PRODUCTION_ROOT}/" || { echo "Failed to change directory"; exit 1; }
+  #echo "Getting a full file list for the root dir"
+  #find . > fullfilelist
+  #if [[ -f /usr/local/bin/create-filelist ]]; then
+  #  # We're already here, but Justin Case wanted this
+  #  cd "${PRODUCTION_ROOT}/" || { echo "Failed to change directory"; exit 1; }
+  #  /bin/cp fullfiletimelist-rocky fullfiletimelist-rocky-old
+  #  /usr/local/bin/create-filelist > fullfiletimelist-rocky
+  #  cp fullfiletimelist-rocky fullfiletimelist
+  #fi
+  ## Full file list update for rocky linux itself
+  #cd "${PRODUCTION_ROOT}/${CATEGORY_STUB}/" || { echo "Failed to change directory"; exit 1; }
+  ## Hardlink everything except xml files
+  #echo "Hard linking"
+  #hardlink -x '.*\.xml.*' "${REVISION}"
+  #echo "Getting a full file list for the rocky dir"
+  #find . > fullfilelist
+  #if [[ -f /usr/local/bin/create-filelist ]]; then
+  #  # We're already here, but Justin Case wanted this
+  #  cd "${PRODUCTION_ROOT}/${CATEGORY_STUB}/" || { echo "Failed to change directory"; exit 1; }
+  #  /bin/cp fullfiletimelist-rocky fullfiletimelist-rocky-old
+  #  /usr/local/bin/create-filelist > fullfiletimelist-rocky
+  #  cp fullfiletimelist-rocky fullfiletimelist
+  #fi
+  #chown 10004:10005 fullfilelist fullfiletimelist-rocky fullfiletimelist
 fi
 
