313a26ec1f186b48331f929ebb644edebd8126a7
Louis Abel <label@rockylinux.org>
2022-07-17 22:11:30 -0700

    split syncs

--- sync/sync-root-file-list.sh
+++ sync/sync-root-file-list.sh
@@ -11,19 +11,16 @@ cd "${STAGING_ROOT}/${CATEGORY_STUB}/${REV}" || { echo "Failed to change directo
 ret_val=$?
 
 if [ $ret_val -eq "0" ]; then
-  # Full file list update for rocky linux itself
-  cd "${PRODUCTION_ROOT}/${CATEGORY_STUB}/" || { echo "Failed to change directory"; exit 1; }
-  # Hardlink everything except xml files
-  #hardlink -x '.*\.xml.*' "${REVISION}"
+  # Full file list update for production root
+  cd "${PRODUCTION_ROOT}/" || echo { echo "Failed to change directory"; exit 1; }
   find . > fullfilelist
   if [[ -f /usr/local/bin/create-filelist ]]; then
     # We're already here, but Justin Case wanted this
-    cd "${PRODUCTION_ROOT}/${CATEGORY_STUB}/" || { echo "Failed to change directory"; exit 1; }
+    cd "${PRODUCTION_ROOT}/" || { echo "Failed to change directory"; exit 1; }
     /bin/cp fullfiletimelist-rocky fullfiletimelist-rocky-old
     /usr/local/bin/create-filelist > fullfiletimelist-rocky
     cp fullfiletimelist-rocky fullfiletimelist
-    cp fullfiletimelist-rocky fullfiletimelist-rocky-linux
   fi
-  chown 10004:10005 fullfilelist fullfiletimelist-rocky fullfiletimelist fullfiletimelist-rocky-linux
+  chown 10004:10005 fullfilelist fullfiletimelist-rocky fullfiletimelist
 fi
 
