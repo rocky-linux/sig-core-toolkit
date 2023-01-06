8e5253f59c5731d4fb6adcb583668982d1d0dfd7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-06 18:25:21 +0000

    Adding tarfile member sanitization to extractall()

--- iso/empanadas/empanadas/util/iso_utils.py
+++ iso/empanadas/empanadas/util/iso_utils.py
@@ -459,7 +459,26 @@ class IsoBuild:
 
         self.log.info('Unpacking %s' % tarball)
         with tarfile.open(tarball) as t:
-            t.extractall(unpack_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(t, unpack_dir)
             t.close()
 
     def _copy_lorax_to_variant(self, force_unpack, arch, image):
