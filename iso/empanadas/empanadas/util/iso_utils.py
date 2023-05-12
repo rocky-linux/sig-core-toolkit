28ba75d019ed5d0a7530131f20f5e6fe70ccee17
Louis Abel <label@rockylinux.org>
2023-05-11 17:52:57 -0700

    fix vars in tree

--- iso/empanadas/empanadas/util/iso_utils.py
+++ iso/empanadas/empanadas/util/iso_utils.py
@@ -369,8 +369,6 @@ class IsoBuild:
                     self.log
             )
 
-        print(latest_artifacts)
-
         self.log.info(Color.INFO + 'Downloading requested artifact(s)')
         for arch in arches_to_unpack:
             lorax_arch_dir = os.path.join(
