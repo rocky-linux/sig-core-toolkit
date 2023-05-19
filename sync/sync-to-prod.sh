ed38b676265b8204dc98ac16f82ddf1b6ef2d63b
Louis Abel <label@rockylinux.org>
2023-05-19 11:30:50 -0700

    add missing dry run flags

--- sync/sync-to-prod.sh
+++ sync/sync-to-prod.sh
@@ -11,7 +11,7 @@ source "$(dirname "$0")/common"
 
 REV=${REVISION}${APPEND_TO_DIR}
 
-cd "${STAGING_ROOT}/${CATEGORY_STUB}/${REV}" || { echo "Failed to change directory"; ret_val=1; exit 1; }
+cd "${STAGING_ROOT}/${CATEGORY_STUB}" || { echo "Failed to change directory"; ret_val=1; exit 1; }
 ret_val=$?
 
 if [ $ret_val -eq "0" ]; then
