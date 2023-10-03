bc72e6c7e468e2378a7b3b6f1afc6077e9f95cb7
Louis Abel <label@rockylinux.org>
2023-10-02 21:16:35 -0700

    fix vault-release.sh messages

--- sync/vault-release.sh
+++ sync/vault-release.sh
@@ -2,6 +2,7 @@
 # Syncs everything from staging to production
 
 if [[ "$RLREL" == "lh" ]] || [[ "$RLREL" == "beta" ]]; then
+  echo "Lookahead nor Beta should be vaulted"
   exit 1
 fi
 
@@ -18,6 +19,6 @@ if [ $ret_val -eq "0" ]; then
   TARGET="${PRODUCTION_ROOT}/${VAULT_STUB}/${REV}"
   mkdir -p "${TARGET}"
   rsync_no_delete_prod "${REV}" "${TARGET}"
-  echo "Syncing to prod completed. Please run the file list script."
+  echo "Syncing to the vault completed. Please run the file list script."
 fi
 
