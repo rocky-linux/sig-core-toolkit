5300649b09e1893710b4278fa69827b297830013
Louis Abel <label@rockylinux.org>
2024-03-19 10:53:55 -0700

    add missing attr install

--- func/core/pkg_acl/00-install-acl.sh
+++ func/core/pkg_acl/00-install-acl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 r_log "acl" "Install the acl package"
 p_installPackageNormal acl
+p_installPackageNormal attr
 # This normally is not needed.
 #r_log "acl" "Remount filesystems with ACL support"
 #mount -o remount,acl /
