7f9ab7d17e860d6623fb031a4326613854bb149b
Louis Abel <label@rockylinux.org>
2025-06-17 19:51:17 -0700

    temp: add quick sync to prod script

---
+++ sync/sync-to-prod-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Syncs every release at once (hardcoded)
+
+if [ -f "sync-to-prod.sh" ]; then
+  echo "Sync to prod script not in PWD."
+  exit 1
+fi
+
+# Rocky Linux 8
+RLVER=8 bash sync-to-prod.sh
+
+# Rocky Linux 9
+RLVER=9 bash sync-to-prod.sh
+
+# Rocky Linux 10
+RLVER=10 bash sync-to-prod.sh
+
+# Updates the file lists
+bash sync-file-list-parallel.sh
+
+# 9 is still in peridot, so there's no downgradable packages
+RLVER=9 bash vault-release-no-repodata.sh
